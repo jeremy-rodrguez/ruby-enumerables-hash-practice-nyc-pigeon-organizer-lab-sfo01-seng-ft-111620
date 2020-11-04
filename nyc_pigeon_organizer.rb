7a88135adaf3657a906061a9ca68f76dfd10f908
Jeremy Rodriguez <jeremy.rodrguez@yahoo.com>
2020-11-04 03:32:13 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,45 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {
+    "Theo" => {
+      :color => ["purple", "grey"],
+      :gender => ["male"],
+      :lives => ["Subway"]
+    },
+    
+    "Peter Jr." => {
+      :color => ["purple", "grey"],
+      :gender => ["male"],
+      :lives => ["Library"]
+    },
+    
+    "Lucky" => {
+      :color => ["purple"],
+      :gender => ["male"],
+      :lives => ["Central Park"]
+    },
+    
+    "Ms. K" => {
+      :color => ["grey", "white"],
+      :gender => ["female"],
+      :lives => ["Central Park"]
+    },
+    
+     "Andrew" => {
+       :color => ["white"],
+       :gender => ["male"],
+       :lives => ["City Hall"]
+     },
+     
+     "Queenie" => {
+       :color => ["white", "brown"],
+       :gender => ["female"],
+       :lives => ["Subway"]
+     },
+     
+     "Alex" => {
+       :color => ["white", "brown"],
+       :gender => ["male"],
+       :lives => ["Central Park"]
+     }
+  }
 end
